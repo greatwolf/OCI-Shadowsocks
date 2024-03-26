f34d79534161510d649df481d84bce823697f51d
greatwolf <github.greatwolf@mamber.net>
2024-03-26 13:56:02 -0700

    Update oci-iprenew.lua

--- oci-iprenew.lua
+++ oci-iprenew.lua
@@ -4,8 +4,8 @@ json = require 'dkjson'
 function oci(...)
   local cmd = table.concat({'oci', ...}, ' ')
   local result = assert(shout(cmd))
-  result = json.decode(result).data or {}
-  return result[1] or result
+  result = json.decode(result)
+  return result and result.data[1] or {}
 end
 
 function dump(t, indent)
