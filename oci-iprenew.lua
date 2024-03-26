9fc32dc2c509aec2c1b9f1d05506b4e48bf65860
greatwolf <github.greatwolf@mamber.net>
2024-03-26 11:55:37 -0700

    Update oci-iprenew.lua

--- oci-iprenew.lua
+++ oci-iprenew.lua
@@ -27,18 +27,23 @@ local res = oci 'iam availability-domain list'.data[1]
 local compartmentid = res["compartment-id"]
 local availdomain   = res.name
 
-print(compartmentid, availdomain)
-
 -- get publicip ocid + address & privateip ocid
 res = oci('network public-ip list',
           '--compartment-id', compartmentid,
           '--scope AVAILABILITY_DOMAIN',
           '--availability-domain', availdomain,
-          '--lifetime', 'EPHEMERAL')
+          '--lifetime', 'EPHEMERAL').data[1]
+local publicocid = res.id
+local privateocid = res['private-ip-id']
 dump(res)
 
 -- unassign current publicip w/o prompting
--- oci network public-ip delete --force --public-ip-id ocid1.publicip.oc1.ap-osaka-1.amaaaaaawl5wgbqaor6wq2a3ukag4cyvuta4xypphsk4ipxmol7367vkjqpq
+oci('network public-ip delete',
+    '--force',
+    '--public-ip-id', publicocid)
 
 -- reassign new publicip
--- oci network public-ip create --compartment-id ocid1.tenancy.oc1..aaaaaaaagyi66dc4o2cawmj4fclkpffuc6igwpx5aqh34mrcyx6qof6trs7a --lifetime EPHEMERAL --private-ip-id ocid1.privateip.oc1.ap-osaka-1.abvwsljrnpnujpkuzbdmem22noq6ik4bhhvoxi3xemisl6f2mji6xisk3qna --debug
+oci('network public-ip create',
+    '--compartment-id', compartmentid,
+    '--lifetime', 'EPHEMERAL',
+    '--private-ip-id', privateocid)
