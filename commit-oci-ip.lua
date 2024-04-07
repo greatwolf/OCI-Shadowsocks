72be24f23deac9ee8221b20f80856ad21f0dfbd8
greatwolf <github.greatwolf@mamber.net>
2024-04-06 23:24:17 -0700

    Added script to commit updated oci public ip back to repo.

---
+++ commit-oci-ip.lua
@@ -0,0 +1,22 @@
+require 'util.sh'
+require 'util.oci'
+require 'util.dump'
+json = require 'dkjson'
+
+-- get compartment-id & availability-domain
+local res = oci 'iam availability-domain list'
+local compartmentid = res["compartment-id"]
+local availdomain   = res.name
+
+-- get public ip address
+res = oci('network public-ip list',
+          '--compartment-id', compartmentid,
+          '--scope AVAILABILITY_DOMAIN',
+          '--availability-domain', availdomain,
+          '--lifetime', 'EPHEMERAL')
+local publicip = res['ip-address']
+
+local cmd    = "curl --key $HOME/.oci/oci_api_key.pem sftp://ubuntu@%s/etc/shadowsocks-rust/config.json"
+local config = shout(cmd:format(publicip))
+config = assert(json.decode(config))
+dump(config)
