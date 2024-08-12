4480a7c43159730aa49c8eac6cd9e802caf658b6
greatwolf <github.greatwolf@mamber.net>
2024-08-11 22:04:08 -0700

    Refactored 'commit-oci-ip'.

--- commit-oci-ip.lua
+++ commit-oci-ip.lua
@@ -6,46 +6,55 @@ b64   = require 'base64'
 json  = require 'dkjson'
 
 -- get compartment-id & availability-domain
-local res = oci 'iam availability-domain list'
-local compartmentid = res["compartment-id"]
-local availdomain   = res.name
+local iam = table.remove(oci 'iam availability-domain list')
+local compartmentid = iam["compartment-id"]
+local availdomain   = iam.name
 
--- get public ip address
-res = oci('network public-ip list',
-          '--compartment-id', compartmentid,
-          '--scope AVAILABILITY_DOMAIN',
-          '--availability-domain', availdomain,
-          '--lifetime', 'EPHEMERAL')
+-- filter out servers not running Shadowsocks
+local instances = oci('compute instance list-vnics',
+                      '--compartment-id', compartmentid)
+                  :filter(function(v)
+                    return  v['public-ip'] and v['display-name']:match '^Shadowsocks'
+                  end)
 
--- get the compute instance's port(usually 443) and shadowsocks password
-local ip  = res['ip-address']
-local cmd = "curl --insecure --key $HOME/.oci/oci_api_key.pem sftp://ubuntu@%s/etc/shadowsocks-rust/config.json"
-local config = shout(cmd:format(ip))
-config = assert(json.decode(config))
+-- get compute instance's shadowsocks config
+local ss_uri  = instances
+                :map(function(v)
+                  local ip = v['public-ip']
+                  local cmd = "curl -s --insecure --key $HOME/.oci/oci_api_key.pem sftp://ubuntu@%s/etc/shadowsocks-rust/config.json"
+                  local config = shout(cmd:format(ip))
+                  config = assert(json.decode(config))
+                  config.ip = ip
+                  return config
+                end)
+                :map(function(config)
+                  -- encode SIP002 URI
+                  local port  = config.server_port
+                  local userinfo  = b64.encode(config.method .. ':' .. config.password)
+                  local prefix    = "%16%03%01%00%C2%A8%01%01" -- TLS ClientHello
 
-local port  = config.server_port
-local userinfo  = b64.encode(config.method .. ':' .. config.password)
-local prefix    = "%16%03%01%00%C2%A8%01%01" -- TLS ClientHello	
-local ss_uri    = string.format("ss://%s@%s:%d/?outline=1&prefix=%s", userinfo, ip, port, prefix)
+                  return string.format("ss://%s@%s:%d/?outline=1&prefix=%s", userinfo, config.ip, port, prefix)
+                end)
 
+-- write SIP002 URI to file and print to stdout
+local file = assert(io.open('./oci-osaka.txt', 'wb'))
 local urimsg = table.concat
 {
   '\27[33;92m',
   [[
     -================================================================================================-
-    |  Shadow Socks SIP002 URI:                                                                      |
-    |    %-91.91s |
+    Shadow Socks SIP002 URI:
+    %-s
     -================================================================================================-
   ]],
   '\27[33;0m'
 }
+ss_uri = table.concat(ss_uri, '\n')
 print(urimsg:format(ss_uri))
-
--- commit updated ip to repo
-local file = assert(io.open('./oci-osaka.txt', 'wb'))
 file:write(ss_uri)
 file:close()
 
+-- commit updated ip to repo
 sh 'git status'
 sh 'git add *.txt'
 sh 'git commit -m "Updated oci public ip"'
