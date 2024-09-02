cb2f057ee7b4371af8f0d5344d9f2d47995c5ea7
greatwolf <github.greatwolf@mamber.net>
2024-09-02 15:48:33 -0700

    Tidied up some code. Upped shape-config core and memory to max allowed.

--- oci-auto-A1.Flex.lua
+++ oci-auto-A1.Flex.lua
@@ -10,34 +10,41 @@ local config =
   ['subnet-id'] = "ocid1.subnet.oc1.iad.aaaaaaaaj4c6vgrtt7e6sxkpesduef6bkavbgvpd2gxig72xnhqto5knla5q",
   ['image-id'] = "ocid1.image.oc1.iad.aaaaaaaai42i6avvfxqawj3bjl5uzhlyq5lqkqhbeg4lpo5corvwqgnvrloq",
   ['shape'] = "VM.Standard.A1.Flex",
-  ['shape-config'] = [['{"memoryInGBs": 16.0,"ocpus": 2.0}']]
+  ['shape-config'] = [['{"memoryInGBs": 24.0,"ocpus": 4.0}']]
 }
-local params = {}
+local function stringify(t)
+  local params = {}
 
-for k, v in pairs(config) do
-  local p = ("--%s %s"):format(k, v)
-  table.insert(params, p)
+  for k, v in pairs(t) do
+    local p = ("--%s %s"):format(k, v)
+    table.insert(params, p)
+  end
+  return table.concat(params, ' ')
 end
 
-local logfmt = "%s %s Status: %d Message: %s"
-function log(resp)
-  local line  = logfmt:format(os.date '[%m/%d %H:%M:%S]',
-                              resp.request_endpoint,
-                              resp.status,
-                              resp.message)
-  print(line)
+local log do
+  local logfmt = "%s %s Status: %d Message: %s"
+  log = function(resp)
+    local line  = logfmt:format(os.date '[%m/%d %H:%M:%S]',
+                                resp.request_endpoint,
+                                resp.status,
+                                resp.message)
+    print(line)
+  end
 end
 
+local params = stringify(config)
 local res = oci('compute instance launch',
                 '--no-retry',
-                table.concat(params, ' '))
+                params)
 log(res)
 
 while res.message == "Out of host capacity." do
   sh 'sleep 30'
   res = oci('compute instance launch',
             '--no-retry',
-            table.concat(params, ' '))
+            params)
   log(res)
 end
+print "*** Stopping Retry ***"
 dump(res)
