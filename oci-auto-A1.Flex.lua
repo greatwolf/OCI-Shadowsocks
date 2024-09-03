263206a73b121239020aaa5481bdb54402ccd644
greatwolf <github.greatwolf@mamber.net>
2024-09-02 20:33:33 -0700

    Removed hard-coded parameters. Made oci-auto-A1.Flex.lua a bit more flexible. It now queries for the needed parameters based on what's in '~/.oci/config'.

--- oci-auto-A1.Flex.lua
+++ oci-auto-A1.Flex.lua
@@ -3,15 +3,40 @@ require 'util.sh'
 require 'util.oci'
 require 'util.dump'
 
-local config =
-{
-  ['compartment-id'] = "ocid1.tenancy.oc1..aaaaaaaasp7dsmvw3jxm5qzk7i5e4tjx7v3os36lshkdjragdyjgj5h53mrq",
-  ['availability-domain'] = "yvnv:US-ASHBURN-AD-1",
-  ['subnet-id'] = "ocid1.subnet.oc1.iad.aaaaaaaaj4c6vgrtt7e6sxkpesduef6bkavbgvpd2gxig72xnhqto5knla5q",
-  ['image-id'] = "ocid1.image.oc1.iad.aaaaaaaai42i6avvfxqawj3bjl5uzhlyq5lqkqhbeg4lpo5corvwqgnvrloq",
-  ['shape'] = "VM.Standard.A1.Flex",
-  ['shape-config'] = [['{"memoryInGBs": 24.0,"ocpus": 4.0}']]
-}
+local attach_subnetid do
+  local subnetid
+  attach_subnetid = function(config)
+    if not subnetid then
+      subnetid  = oci('network subnet list',
+                      '--all',
+                      '--compartment-id', config['compartment-id'])
+      subnetid = table.remove(subnetid)
+      subnetid = assert(subnetid and subnetid.id)
+    end
+    config['subnet-id'] = subnetid
+    return config
+  end
+end
+
+local attach_imageid do
+  local imageid
+  attach_imageid = function(config)
+    if not imageid then
+      imageid = oci('compute image list',
+                    '--all',
+                    '--compartment-id', config['compartment-id'])
+                :filter(function(v)
+                  return  v['operating-system'] == "Canonical Ubuntu"
+                          and v['operating-system-version']:match "Minimal aarch64$"
+                end)
+      imageid = table.remove(imageid, 1)
+      imageid = assert(imageid and imageid.id)
+    end
+    config['image-id'] = imageid
+    return config
+  end
+end
+
 local function stringify(t)
   local params = {}
 
@@ -21,6 +46,19 @@ local function stringify(t)
   end
   return table.concat(params, ' ')
 end
+
+local iam = oci 'iam availability-domain list'
+local configs = iam
+                :map(function(v)
+                  v.id = nil
+                  v['availability-domain'], v.name, v.id = v.name
+                  v.shape = "VM.Standard.A1.Flex"
+                  v['shape-config'] = [['{"memoryInGBs": 24.0,"ocpus": 4.0}']]
+                  return v
+                end)
+                :map(attach_subnetid)
+                :map(attach_imageid)
+                :map(stringify)
 
 local log do
   local logfmt = "%s %s Status: %d Message: %s"
@@ -33,18 +71,22 @@ local log do
   end
 end
 
-local params = stringify(config)
-local res = oci('compute instance launch',
-                '--no-retry',
-                params)
-log(res)
+local function pick(configs)
+  local i = math.random(1, #configs)
+  return configs[i]
+end
 
-while res.message == "Out of host capacity." do
-  sh 'sleep 60'
+local res
+while true do
   res = oci('compute instance launch',
             '--no-retry',
-            params)
+            pick(configs))
   log(res)
+  if res.message ~= "Out of host capacity." then
+    break
+  end
+  sh 'sleep 60'
 end
+
 print "*** Stopping Retry ***"
 dump(res)
